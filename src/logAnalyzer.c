8a2b2e05aa26b1444cb1f62d0e04aac7d760797a
Tiago Costa Chousal <2024116801@ufp.edu.pt>
2026-03-26 11:27:48 +0000

    Updates e requisito 3.2 feito

--- src/logAnalyzer.c
+++ src/logAnalyzer.c
@@ -3,13 +3,13 @@
 void parseArguments(int argc, char *argv[], CONFIG *config){
     if (argc<4){
         printf("Formato inválido!!\n");
-        printf("Tente algo como: ./logAnalyzer /var/log/apache2 4 security --verbose --output=report.json");
+        fprintf(stderr,"Tente algo como: ./logAnalyzer /var/log/apache2 4 security --verbose --output=report.json",argv[0]);
         exit(EXIT_FAILURE);
     }
     config->diretorio = argv[1];
     config->numProcessos = atoi(argv[2]);
     if(config->numProcessos <=0){
-        printf("Tem de haver pelo menos um processo");
+        fprintf(stderr,"Tem de haver pelo menos um processo");
         exit(EXIT_FAILURE);
     }
     
@@ -40,4 +40,93 @@ void parseArguments(int argc, char *argv[], CONFIG *config){
         }
     }
     
+}
+void logWorker(CONFIG *config) {
+    int nWorkers = config->numProcessos;
+    __pid_t pids[nWorkers];
+    int fdsDuplo[nWorkers][2]; 
+
+    // recrutmento de workers
+    for(int i = 0; i < nWorkers; i++) {
+        
+        // criaçao do pipe
+        if (pipe(fdsDuplo[i]) == -1) {
+            perror("Erro ao criar o pipe\n");
+            exit(EXIT_FAILURE);
+        }
+
+        pids[i] = fork();
+        
+        if (pids[i] == -1) {
+            perror("Erro ao criar o fork\n");
+            exit(EXIT_FAILURE);
+        } 
+        
+        
+        if (pids[i] == 0) {
+            close(fdsDuplo[i][1]); // porta de escrita fechada
+
+           
+            for (int j = 0; j < i; j++) {
+                close(fdsDuplo[j][0]);
+                close(fdsDuplo[j][1]);
+            }
+
+            char buffer[1024];
+            ssize_t bytesLidos;
+            
+            while ((bytesLidos = read(fdsDuplo[i][0], buffer, sizeof(buffer) - 1)) > 0) {
+                buffer[bytesLidos] = '\0';
+                printf("Worker %d recebeu: %s", i, buffer);
+            }
+            
+            close(fdsDuplo[i][0]); // porta de leitura fechada
+            exit(EXIT_SUCCESS);    
+        } 
+        
+        else {
+            close(fdsDuplo[i][0]); // Pai fecha a ponta de leitura
+        }
+    }
+
+    
+    int fdLog = open(config->diretorio, O_RDONLY);
+    if(fdLog == -1){
+        perror("Erro ao abrir a log\n");
+        exit(EXIT_FAILURE);
+    } 
+
+    char c;
+    char linhaBuffer[2048];
+    int pos = 0;
+    int workerAtual = 0;
+
+    
+    while (read(fdLog, &c, 1) > 0) {
+        linhaBuffer[pos] = c;
+        pos++;
+        
+        if (c == '\n') {
+            write(fdsDuplo[workerAtual][1], linhaBuffer, pos);
+            pos = 0;
+            
+            workerAtual++;
+            if (workerAtual >= nWorkers) { 
+                workerAtual = 0;
+            }
+        }
+    }
+    close(fdLog);
+
+    // o pai fecha o pipe porque o ficheiro já acabou
+    for (int i = 0; i < nWorkers; i++) {
+        close(fdsDuplo[i][1]);
+    }
+
+    // ciclo para evitar zombies
+    for (int i = 0; i < nWorkers; i++) {
+        wait(NULL);
+    }
+           
+    printf("\n[Director] Todos os workers acabaram com sucesso\n");
 }
