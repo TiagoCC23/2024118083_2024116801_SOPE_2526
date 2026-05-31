e26a68a4b5ce484c83a060386805e3159c148ac7
TiagoCC23 <2024116801@ufp.edu.pt>
2026-05-31 23:16:11 +0100

    Back do back

--- src/main.c
+++ src/main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 #include "R3_1_logAnalyzer.h"
 #include "R3_2_workers.h"
-#include "R3_3_workers_pipes.h"
-#include "R3_4_R4_2_dashboard.h"
-#include "R3_5_workers_sockets.h"
 #include "R4_1_threads.h"
+#include "R3_3_workers_pipes.h"
+#include "R3_5_workers_sockets.h"
 #include "R4_3_prodConsBoundedBuffer.h"
-
+#include "R3_4_R4_2_dashboard.h"
 
 int main(int argc, char *argv[]){
     CONFIG config;
@@ -22,36 +20,19 @@ int main(int argc, char *argv[]){
     printf("Modo escolhido: %d\n",config.modo);
     printf("Modo Verboso: %s\n", config.verbose ? "Ligado" : "Desligado");
     
-    if(config.numProdutores > 0 && config.numConsumidores > 0){
-        printf("[Execução] Ativando Modelo Produtor-Consumidor...\n");
-        logWorkerProducerConsumer(&config);
-    } 
+    if(config.outFiles){
+        printf("A guardar ficheiro em: %s\n", config.outFiles);
+    }
+    if(config.numProcessos > 0 && config.numThreads == 0){
+        //logWorker_pipes(&config);
+        //logWorker(&config); 
+        //logWorker_sockets(&config);
 
-    else if(config.numThreads > 0){
-        printf("[Execução] Ativando Modelo de Worker Threads...\n");
+    } else if(config.numThreads > 0){
         logWorkerThreads(&config);
-    } 
-   
-    else if(config.numProcessos > 0){
-        if (strcmp(config.ipc_mode, "basic") == 0) {
-            printf("Multi-Processo - basic - (Req. B)...\n");
-            logWorker(&config);
-        } else if (strcmp(config.ipc_mode, "pipes") == 0) {
-            printf("Comunicação via Pipes (Req. C)...\n");
-            logWorker_pipes(&config);
-        } else if (strcmp(config.ipc_mode, "dashboard") == 0) {
-            printf("Dashboard de Processos (Req. D)...\n");
-            logWorker_dashboard(&config);
-        } else if (strcmp(config.ipc_mode, "sockets") == 0) {
-            printf("Comunicação via Unix Domain Sockets (Req. E)...\n");
-            logWorker_sockets(&config);
-        } else {
-            perror("Modo IPC '%s' não reconhecido. Opções: basic, pipes, dashboard, sockets.\n");
-            return 1;
-        }
-    } else {
-        perror("Erro de Configuração: Defina num_processos > 0, --threads, ou --produtores/--consumidores.\n");
-        return 1;
+    }
+    if(config.numProdutores > 0 && config.numConsumidores > 0){
+    logWorkerProducerConsumer(&config);
     }
     return 0;
 }
